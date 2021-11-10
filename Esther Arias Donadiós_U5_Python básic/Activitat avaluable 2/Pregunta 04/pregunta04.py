ba0e6df57b75f5b36ab5126dc70541558c4ff59d
Esther Arias <78144940+erasds@users.noreply.github.com>
2021-11-11 00:32:38 +0100

    Add files via upload

--- Esther Arias Donadiós_U5_Python básic/Activitat avaluable 2/Pregunta 04/pregunta04.py
+++ Esther Arias Donadiós_U5_Python básic/Activitat avaluable 2/Pregunta 04/pregunta04.py
@@ -1,9 +1,32 @@
 print("Suelo seguro: 0 ", "Suelo minado: -1")
 print("Pinte la primera línea del escenario: ")
-linea1 = input().split()
+linea1 = input().split(" ")
 print("Pinte la segunda línea del escenario: ")
-linea2 = input().split()
-escenarioMinas = [[linea1],[linea2]]
+linea2 = input().split(" ")
+escenarioMinas = [linea1,linea2]
 print(escenarioMinas)
 
-# def contandoMinas(miCampo):
+def contandoMinas(miCampo):
+    for i in range(len(miCampo)):
+        print("\n")
+        for j in range(len(miCampo[i])):
+            suma = 0
+            if i == -1:
+                pass
+            if i - 1 >= 0: # miro a la izquierda
+                if miCampo[i-1][j] == -1:
+                    suma += 1
+            if j - 1 >= 0: # miro arriba
+                if miCampo[i][j-1] == -1:
+                    suma += 1
+            if i + 1 < len(miCampo): # miro a la derecha
+                if miCampo[i+1][j] == -1:
+                    suma += 1
+            if j + 1 < len(miCampo[i]): # miro abajo
+                if miCampo[i][j + 1] == -1:
+                    suma += 1
+            print(suma, end= " ")
+
+contandoMinas(escenarioMinas)
+
+
