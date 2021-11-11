d71a60b2230f551f1bb89ec747769971e2d5830d
Esther Arias <78144940+erasds@users.noreply.github.com>
2021-11-11 22:46:26 +0100

    Add files via upload

--- Esther Arias Donadiós_U5_Python básic/Activitat avaluable 2/Pregunta 04/pregunta04.py
+++ Esther Arias Donadiós_U5_Python básic/Activitat avaluable 2/Pregunta 04/pregunta04.py
@@ -11,21 +11,37 @@ def contandoMinas(miCampo):
         print("\n")
         for j in range(len(miCampo[i])):
             suma = 0
-            if i == -1:
-                pass
-            if i - 1 >= 0: # miro a la izquierda
-                if miCampo[i-1][j] == -1:
-                    suma += 1
-            if j - 1 >= 0: # miro arriba
-                if miCampo[i][j-1] == -1:
-                    suma += 1
-            if i + 1 < len(miCampo): # miro a la derecha
-                if miCampo[i+1][j] == -1:
-                    suma += 1
-            if j + 1 < len(miCampo[i]): # miro abajo
-                if miCampo[i][j + 1] == -1:
-                    suma += 1
-            print(suma, end= " ")
+            if miCampo[i][j] == "-1":
+                print("-1", end = " ")
+            else:
+                if i - 1 >= 0: # miro a la izquierda
+                    if miCampo[i-1][j] == "-1":
+                        suma += 1
+                if j - 1 >= 0: # miro arriba
+                    if miCampo[i][j-1] == "-1":
+                        suma += 1
+                if i + 1 < len(miCampo): # miro a la derecha
+                    if miCampo[i+1][j] == "-1":
+                        suma += 1
+                if j + 1 < len(miCampo[i]): # miro abajo
+                    if miCampo[i][j + 1] == "-1":
+                        suma += 1
+                if i - 1 & j - 1 >= 0: # diagonal noroeste
+                    if miCampo[i-1][j-1] == "-1":
+                        suma += 1
+                if i + 1 < len(miCampo): # diagonal noreste
+                    if j - 1 >= 0:
+                        if miCampo[i+1][j-1] == "-1":
+                            suma += 1
+                if i - 1 >= 0: # diagonal suroeste
+                    if j + 1 < len(miCampo[i]):
+                        if miCampo[i-1][j+1] == "-1":
+                            suma += 1
+                if i + 1 < len(miCampo): # diagonal sureste
+                    if j + 1 >= 0:
+                        if miCampo[i+1][j+1] == "-1":
+                            suma += 1
+                print(suma, end= " ")
 
 contandoMinas(escenarioMinas)
 
