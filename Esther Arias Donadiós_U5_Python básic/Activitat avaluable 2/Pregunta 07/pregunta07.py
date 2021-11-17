60366777975120e63b8310fe775d7813ae60a074
Esther Arias <78144940+erasds@users.noreply.github.com>
2021-11-17 23:37:43 +0100

    Add files via upload

--- Esther Arias Donadiós_U5_Python básic/Activitat avaluable 2/Pregunta 07/pregunta07.py
+++ Esther Arias Donadiós_U5_Python básic/Activitat avaluable 2/Pregunta 07/pregunta07.py
@@ -1,45 +1,56 @@
 import sys
 import os
 """ACUERDATE!! comprobar con test unitari... ??? y sacar ejecutable"""
-# leemos el fichero de entrada, cuyo nombre hemos recibido por consola como primer parámetro
 
 fEntrada = open(sys.argv[1], "r")
 fSalida = open(sys.argv[2], "w")
 
-
-def esPalindromo(fichero):
-    numsPalindromos = []
-    numerosInvertidos = []
+def leerLineas(fichero):
     lineas = fichero.readlines()
     lineas = [i.strip() for i in lineas]
-    numeros = list(map(str, lineas))
-    for num in range(len(numeros)):
+    numeros = list(map(int, lineas))
+    return numeros
+
+
+def esPalindromo():
+    numsPalindromos = []
+    numerosInvertidos = []
+    cadenaNumeros = list(map(str, leerLineas(fEntrada)))
+    for num in range(len(cadenaNumeros)):
         numInvertido = ""
-        numInvertido = numeros[num][::-1]
+        numInvertido = cadenaNumeros[num][::-1]
         numerosInvertidos.append(numInvertido)
-    for i in range(len(numeros)):
+    for i in range(len(cadenaNumeros)):
         for j in range(len(numerosInvertidos)):
-            if numeros[i] == numerosInvertidos[j]:
-                numsPalindromos.append(numeros[i])
+            if cadenaNumeros[i] == numerosInvertidos[j]:
+                numsPalindromos.append(cadenaNumeros[i])
     numerosPalindromos = list(map(int, numsPalindromos))
     return numerosPalindromos
 
-def esPrimo(fichero):
+def esPrimo():
     numerosPrimos = []
-    numeros = fichero.readlines()
+    numeros = leerLineas(fEntrada)
+    print(numeros)
     for i in range(len(numeros)):
-        if i > 2 and numeros[i] % i != 0:
-            numerosPrimos.append(numeros[i])
+        if numeros[i] > 1:
+            for n in range(2, numeros[i]):
+                if numeros[i] % n == 0:
+                    print("entra al if con ", numeros[i] + "y de rango n: ", n)
+                    continue
+                else:
+                    print("entra al else imprimimos numeros[i]", numeros[i])
+                    numerosPrimos.append(numeros[i])
+                    print("e imprimo numerosPrimos que se supone que acaba de añadir un numero", numerosPrimos)
     print(numerosPrimos)
     return numerosPrimos
 
 
 def imprimirResultado(ficheroSalida):
     ficheroSalida = open(sys.argv[2],"w")
-    ficheroSalida.write("Hay %s números palíndromos." % (len(esPalindromo(fEntrada))) + os.linesep)
-    ficheroSalida.write("Hay %s números primos." % (len(esPrimo(fEntrada))) + os.linesep)
-    palindromosOrdenados = sorted(esPalindromo(fEntrada))
-    primosOrdenados = sorted(esPrimo(fEntrada))
+    ficheroSalida.write("Hay %s números palíndromos." % (len(esPalindromo())) + os.linesep)
+    ficheroSalida.write("Hay %s números primos." % (len(esPrimo())) + os.linesep)
+    palindromosOrdenados = sorted(esPalindromo())
+    primosOrdenados = sorted(esPrimo())
     for i in range(len(palindromosOrdenados)):
         for j in range(len(primosOrdenados)):
             if palindromosOrdenados[i] == primosOrdenados[j]:
