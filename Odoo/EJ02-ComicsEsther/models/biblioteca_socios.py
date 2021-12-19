cd7d206bffabf03ab0cdb78a0f6939dab1430a7c
Esther Arias <78144940+erasds@users.noreply.github.com>
2021-12-20 00:54:53 +0100

    Add files via upload

---
+++ Odoo/EJ02-ComicsEsther/models/biblioteca_socios.py
@@ -0,0 +1,20 @@
+from odoo import models, fields, api    
+
+class BibliotecaSocios(models.Model):
+    #Nombre y descripción del modelo
+    _name = 'biblioteca.socios'
+
+    _description = 'Socios de biblioteca'
+
+    #Parametros de ordenación por defecto
+    _order = 'id, apellido, nombre_ids'
+
+    #ATRIBUTOS
+    nombre_ids = fields.Char("Nombre")
+    apellido = fields.Char("Apellido")
+    id = fields.Integer("Id")
+
+    #Constraints de SQL del modelo
+    _sql_constraints = [
+        ('id_uniq', 'UNIQUE (id)', 'El id del socio debe ser único'),
+    ]
