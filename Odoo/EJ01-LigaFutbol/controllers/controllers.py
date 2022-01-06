e7e49816a83bb67347f4c9fc49495781008bd166
Esther Arias <78144940+erasds@users.noreply.github.com>
2022-01-06 19:14:14 +0100

    Add files via upload

---
+++ Odoo/EJ01-LigaFutbol/controllers/controllers.py
@@ -0,0 +1,7 @@
+"""class MyController(http.Controller):
+    @http.route('/eliminarempates', auth='user', type='json')
+    def elimina_empates(self):
+        for record in self:
+            if record.goles_casa == record.goles_fuera:"""
+
+
