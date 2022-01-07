a5da2aeef576e6454a43befa01730d8df0f95660
Esther Arias <78144940+erasds@users.noreply.github.com>
2022-01-07 20:54:59 +0100

    Delete Odoo/EJ04-Instituto directory

--- Odoo/EJ04-Instituto/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# Indicamos que se importe el contenido Python de la carpeta "models" y "controllers"
-from . import models
-from . import controllers
--- Odoo/EJ04-Instituto/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Indicamos los ficheros que se van a cargar
-from . import instituto_ciclos
-from . import instituto_modulos
-from . import instituto_alumnos
-from . import instituto_profesores
