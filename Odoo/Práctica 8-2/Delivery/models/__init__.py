7d412824579236afaf5078cb605a4e5c38dcfe2d
Esther Arias <78144940+erasds@users.noreply.github.com>
2022-01-13 13:47:57 +0100

    Add files via upload

---
+++ Odoo/Práctica 8-2/Delivery/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from . import delivery_empleados
+
+# Aqui indicamos que se cargarán los ficheros de los modelos
---
+++ Odoo/Práctica 8-2/Delivery/models/delivery_empleados.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+#Definimos modelo Delivery Empleados, que almacenará información de los empleados
+class DeliveryEmpleados(models.Model):
+
+    #Nombre y descripción del modelo
+    _name = 'delivery.empleados'
+
+    _description = 'Empleados de la empresa'
+
+    #Parámetros de ordenación por defecto
+    _order = 'nombre', 'apellidos'
+
+    #ATRIBUTOS
+
+    # el atributo del que cogerá el nombre
+    _rec_name = 'nombre'
+
+    # nombre del empleado
+    nombre = fields.Char("Nombre")
+    # apellidos del empleado
+    apellidos = fields.Char("Apellidos")
+    # dni del empleado, que debe ser único
+    dni = fields.Integer("DNI")
+    # teléfono del empleado
+    telf = fields.Integer("Teléfono")
+    # carnet del empleado donde podemos seleccionar entre varias opciones
+    carnet = fields.Selection(selection=[
+        ('moto', 'Carnet de moto'),
+        ('furgoneta', 'Carnet de furgoneta'),
+        ('ambos', 'Carnet de moto y furgoneta'),
+        ('ninguno', 'No tiene carnet')
+    ], string="Tipo de carnet", default='ninguno')
+    #Dato binario, para guardar un binario (en la vista indicaremos que es una imagen) con la foto del empleado
+    foto = fields.Image('Avatar', max_width=200,max_height=200)
+    
+
+
+    #Constraints de SQL del modelo, para controlar que el dni sea único
+    _sql_constraints = [
+        ('dni_uniq', 'UNIQUE (dni)', 'El dni del empleado debe ser único')
+    ]
